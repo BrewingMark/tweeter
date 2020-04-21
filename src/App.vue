ec6321957d48637761565950846c57cfb05817de
Mark Nicol <marknicol1991@live.co.uk>
2020-04-21 21:37:19 +0100

    MVP Complete.

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,61 @@
-<template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+<template lang="html">
+  <div class="">
+
+    <h1>Tweets</h1>
+    <h2>Total Likes:{{totalLikes}}</h2>
+    <tweet-list-item v-for="(tweet, index) in tweets" :key="index" :tweet="tweet"></tweet-list-item>
+
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import TweetListItem from './components/TweetListItem.vue';
 
 export default {
-  name: 'App',
+  name: 'app',
+  data() {
+  return {
+    tweets:[
+      {
+        id: 1,
+        name: 'James',
+        handle: '@jokerjames',
+        img: 'https://semantic-ui.com/images/avatar2/large/matthew.png',
+        tweet: "If you don't succeed, dust yourself off and try again.",
+        likes: 10,
+      },
+      {
+        id: 2,
+        name: 'Fatima',
+        handle: '@fantasticfatima',
+        img: 'https://semantic-ui.com/images/avatar2/large/molly.png',
+        tweet: 'Better late than never but never late is better.',
+        likes: 12,
+      },
+      {
+        id: 3,
+        name: 'Xin',
+        handle: '@xeroxin',
+        img: 'https://semantic-ui.com/images/avatar2/large/elyse.png',
+        tweet: 'Beauty in the struggle, ugliness in the success.',
+        likes: 18,
+      }
+    ]
+  };
+},
+computed: {
+  totalLikes: function(){
+  return this.tweets.reduce((runningTotal, tweet) => {
+    return runningTotal + tweet.likes;
+  }, 0);
+}
+},
   components: {
-    HelloWorld
+    'tweet-list-item': TweetListItem
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="css" scoped>
 </style>
---
+++ src/components/TweetListItem.vue
@@ -0,0 +1,40 @@
+<template lang="html">
+  <li><img v-bind:src="tweet.img" alt="Image of User"><p>{{tweet.name}} {{tweet.handle}}</p>
+    <h3>{{tweet.tweet}}</h3><br>
+    <p>Likes:{{tweet.likes}}</p>
+  </li>
+</template>
+
+<script>
+export default {
+  name: 'tweet-list-item',
+  props: ['tweet']
+}
+</script>
+
+<style lang="css" scoped>
+
+li {
+  background-color: #30475e;
+  color: white;
+  list-style: none;
+}
+
+h3 {
+  margin-left: 45px;
+}
+
+p {
+  margin-left: 10px;
+}
+
+img {
+  border-radius: 50%;
+  height: 8%;
+  width: 8%;
+  margin-top: 5px;
+  margin-left: 5px;
+
+}
+
+</style>
